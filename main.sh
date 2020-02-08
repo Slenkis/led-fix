249ecbed81022c5c219ffd936ef142907144e6aa
Slenkis <30866426+Slenkis@users.noreply.github.com>
2020-02-08 23:53:29 +0300

    Add files via upload

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 
 curl https://raw.githubusercontent.com/Slenkis/led-fix/master/ledfix.sh > /etc/init.d/ledfix
 chmod +x /etc/init.d/ledfix
-/etc/init.d/ledfix enable
+/etc/init.d/ledfix enable --now
