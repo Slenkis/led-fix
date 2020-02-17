a9b659a0f20c535482a797becdaa8fe33f36577b
Slenkis <30866426+Slenkis@users.noreply.github.com>
2020-02-18 00:25:30 +0300

    review code

--- ledfix.sh
+++ ledfix.sh
@@ -4,19 +4,21 @@ START=97
 
 start() {
 
+sys="/sys/class/leds/mir3g:amber"
+
 ## WAN
-echo 0x2 > /sys/class/leds/mir3g:amber:wan/port_mask
-echo 0xf > /sys/class/leds/mir3g:amber:wan/speed_mask
-echo link > /sys/class/leds/mir3g:amber:wan/mode
+echo 0x2 > $sys:wan/port_mask
+echo 0xf > $sys:wan/speed_mask
+echo link > $sys:wan/mode
 
 ## LAN 1
-echo 0x8 > /sys/class/leds/mir3g:amber:lan1/port_mask
-echo 0xf > /sys/class/leds/mir3g:amber:lan1/speed_mask
-echo link > /sys/class/leds/mir3g:amber:lan1/mode
+echo 0x8 > $sys:lan1/port_mask
+echo 0xf > $sys:lan1/speed_mask
+echo link > $sys:lan1/mode
 
 ## LAN 2
-echo 0x4 > /sys/class/leds/mir3g:amber:lan2/port_mask
-echo 0xf > /sys/class/leds/mir3g:amber:lan2/speed_mask
-echo link > /sys/class/leds/mir3g:amber:lan2/mode
+echo 0x4 > $sys:lan2/port_mask
+echo 0xf > $sys:lan2/speed_mask
+echo link > $sys:lan2/mode
 
 }
